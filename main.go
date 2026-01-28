6fb7aa8a3b47382c0d46b3e846138715f2374dad
Ondřej Malina <ondrej.malina@outlook.com>
2026-01-28 13:22:33 +0000

    feat: add basic cobra app

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "beloved",
+	Short: "A simple CLI tool for tracking favorite (or 'beloved') filesystem paths",
+	Long: `beloved is a CLI tool for tracking of favorite filesystem paths.
+You can add or remove favorite paths to it using commands.`,
+	Run:  greeting,
+}
+
+func greeting(cmd *cobra.Command, args []string) {
+	fmt.Println("beloved!<3")
+}
 
 func main() {
-	fmt.Println("beloved <3!")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
