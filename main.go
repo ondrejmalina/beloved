177efab04e25791806da6c3519a0f42d6b014dff
Ondřej Malina <ondrej.malina@outlook.com>
2026-02-14 05:53:46 +0000

    feat: store progress

--- main.go
+++ main.go
@@ -26,6 +26,34 @@ You can add or remove favorite paths to it using commands.`,
 	Run: greeting,
 }
 
+func greeting(cmd *cobra.Command, args []string) {
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Title("Beloved ❤️").
+				Options(huh.NewOptions("United States", "Canada", "Mexico")...).
+				Value(&testPath),
+		),
+	)
+
+	err := form.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+type config struct {
+	path 	string
+	beloved []string
+}
+
 func getDefaultConfigPath() (string, error) {
 	dd, err := os.UserConfigDir()
 	if err != nil {
@@ -35,29 +63,17 @@ func getDefaultConfigPath() (string, error) {
 	return path, nil
 }
 
-func createDefaultConfig(path string) (string, error) {
+func createDefaultConfig(path string) error {
 	err := os.MkdirAll(path, 0660)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	f, err := os.Create(filepath.Join(path, default_cfg_file))
+	f.Close()
 	if err != nil {
-		return nil, err
+		return err
 	}
-	defer f.Close()
-
-	return f.Name(), nil
-}
-
-func openConfig(path string) (*os.File, error) {
-	cfg, err := os.Open(path)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return cfg, nil
 }
 
 // Reads cfg file and return a slice of beloved paths.
@@ -84,48 +100,40 @@ func readConfig(path string) ([]string, error) {
 // default path, it creates it.
 func loadConfig(path string) ([]string, error) {
 	if path == "" {
-		p, err := getDefaultConfigPath()
+		cfg, err := loadDefaultConfig()
 		if err != nil {
+			return nil, err
 		}
 	}
 
-	return nil, nil
-}
-
-func loadDefaultConfig() error {
-	p, err := getDefaultConfigPath()
+	cfg, err := readConfig(path)
 	if err != nil {
-		return err
+		return nil, fmt.Errorf("failed to read config on path %s: %w", path, err)
 	}
-
-	fp := filepath.Join(p, default_cfg_file)
-
-	data, err := os.ReadFile(fp)
-	if err != nil {
-		return nil, err
-	}
+	return cfg, nil
 
 }
 
-func greeting(cmd *cobra.Command, args []string) {
-	form := huh.NewForm(
-		huh.NewGroup(
-			huh.NewSelect[string]().
-				Title("Beloved ❤️").
-				Options(huh.NewOptions("United States", "Canada", "Mexico")...).
-				Value(&testPath),
-		),
-	)
-
-	err := form.Run()
+func loadDefaultConfig() ([]string, error) {
+	dp, err := getDefaultConfigPath()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("failed to get default cfg path: %w", err)
 	}
-}
 
-func main() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	fp := filepath.Join(dp, default_cfg_file)
+	cfg, err := readConfig(fp)
+
+	if err == nil {
+		return cfg, nil
 	}
+
+	if errors.Is(err, os.ErrNotExist) {
+		err = createDefaultConfig(fp)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create default config: %w", err)
+		}
+		return []string{}, nil
+	}
+
+	return nil, fmt.Errorf("failed to read config at %s: %w", fp, err)
 }
