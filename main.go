c5cfb70bf8d78ddfe54d8a6c40789001caac487d
Ondrej Malina <ondrejmalina@outlook.com>
2026-02-14 09:45:34 +0100

    feat: store progress

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -12,7 +13,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const default_cfg_file string = "beloved.cfg"
+const (
+	default_cfg_file string = "beloved.cfg"
+	default_cfg_folder string = "beloved"
+)
 
 var (
 	testPath string
@@ -50,90 +54,83 @@ func main() {
 }
 
 type config struct {
-	path 	string
+	path    string // path to the config file
 	beloved []string
 }
 
-func getDefaultConfigPath() (string, error) {
-	dd, err := os.UserConfigDir()
-	if err != nil {
-		return "", err
+// Searches for config on path. If the path is an empty string, loads the
+// config from the OS default config path. If it does not exist on the
+// default path, it creates it.
+func (c *config) loadConfig() error {
+	if c.path == "" {
+		err := c.loadDefaultConfig(); err != nil {
+			return err
+		}
 	}
-	path := filepath.Join(dd, "beloved")
-	return path, nil
+
+	err := c.readConfig(); err != nil {
+		return fmt.Errorf("failed to read config on path %s: %w", c.path, err)
+	}
+	return nil
+
 }
 
-func createDefaultConfig(path string) error {
-	err := os.MkdirAll(path, 0660)
+func (c *config) loadDefaultConfig() error {
+	err := c.getDefaultConfigPath()
+	if err != nil {
+		return fmt.Errorf("failed to get default cfg path: %w", err)
+	}
+
+	err = c.readConfig(); err == nil {
+		return nil
+	}
+
+	if errors.Is(err, os.ErrNotExist) {
+		err = c.createDefaultConfig(); err != nil {
+			return fmt.Errorf("failed to create default config file: %w", err)
+		}
+	}
+
+	err = c.readConfig(); err != nil {
+		return fmt.Errorf("failed to read config on path %s: %w", c.path, err)
+	}
+	return nil
+}
+
+func (c *config) getDefaultConfigPath() error {
+	dd, err := os.UserConfigDir()
+	if err != nil {
+		return err
+	}
+	c.path := filepath.Join(dd, default_cfg_folder, default_cfg_file)
+	return nil
+}
+
+// Reads cfg file from path to configs slice of beloved paths.
+func (c *config) readConfig() error {
+	// TODO: Add validation of correct cfg format
+	data, err := os.ReadFile(c.path)
 	if err != nil {
 		return err
 	}
 
-	f, err := os.Create(filepath.Join(path, default_cfg_file))
+	scanner := bufio.NewScanner(bytes.NewReader(data))
+	for scanner.Scan() {
+		c.beloved = append(c.beloved, scanner.Text())
+	}
+	return nil
+}
+
+func (c *config) createDefaultConfig() error {
+	err := os.MkdirAll(filepath.Dir(c.path), 0660)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Create(c.path)
 	f.Close()
 	if err != nil {
 		return err
 	}
 }
 
-// Reads cfg file and return a slice of beloved paths.
-func readConfig(path string) ([]string, error) {
-	// TODO: Add validation of correct cfg format
-	paths := []string{}
-
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	scanner := bufio.NewScanner(bytes.NewReader(data))
-
-	for scanner.Scan() {
-		paths = append(paths, scanner.Text())
-	}
-
-	return paths, nil
-}
-
-// Searches for config on path. If the path is an empty string, loads the
-// config from the OS default config path. If it does not exist on the
-// default path, it creates it.
-func loadConfig(path string) ([]string, error) {
-	if path == "" {
-		cfg, err := loadDefaultConfig()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	cfg, err := readConfig(path)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read config on path %s: %w", path, err)
-	}
-	return cfg, nil
-
-}
-
-func loadDefaultConfig() ([]string, error) {
-	dp, err := getDefaultConfigPath()
-	if err != nil {
-		return fmt.Errorf("failed to get default cfg path: %w", err)
-	}
-
-	fp := filepath.Join(dp, default_cfg_file)
-	cfg, err := readConfig(fp)
-
-	if err == nil {
-		return cfg, nil
-	}
-
-	if errors.Is(err, os.ErrNotExist) {
-		err = createDefaultConfig(fp)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create default config: %w", err)
-		}
-		return []string{}, nil
-	}
-
-	return nil, fmt.Errorf("failed to read config at %s: %w", fp, err)
-}
